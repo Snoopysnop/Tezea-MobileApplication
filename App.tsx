e8bfb0647055c598cf7d9902c8624b08f380f93c
Hugo Eckenschwiller <hugo.eckenschwiller@etudiant.univ-rennes1.fr>
2024-04-30 10:40:41 +0200

    add provider

--- App.tsx
+++ App.tsx
@@ -1,14 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Test from './screens/test';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Test/>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Test />
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
--- screens/test.tsx
+++ screens/test.tsx
@@ -1,12 +1,14 @@
 import {useSelector, useDispatch} from "react-redux"
 import { RootState } from "../redux/rootReducer"
+import { StyleSheet, Text, View } from 'react-native';
+
 
 const Test: React.FC = () => {
 
     const testReduxData = useSelector((state: RootState) => state.test.data)
 
     return(
-        <div>TEST</div>
+        <Text>TEST</Text>
     )
 }
 
